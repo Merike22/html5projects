1365dabb8ba2d94114850d327a467d39fcf14205
Merike Tomson <merike.tomson@khk.ee>
2016-02-07 23:21:52 +0200

    Lecture 26: Snake Game Scripting Part B [Delivers #113239609]

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     var d = "right";
     var food;
     var score;
+    var color ="blue";
     var speed = 100;
 
     //Snake Array
@@ -15,6 +16,7 @@ $(document).ready(function(){
 
     //Initializer
     function init(){
+        d = "right";
         create_snake();
         create_food();
         score = 0;
@@ -41,6 +43,74 @@ $(document).ready(function(){
             x: Math.round(Math.random()*(w-cw)/cw),
             y: Math.round(Math.random()*(h-cw)/cw),
         };
+    }
+
+    //Paint Snake
+    function paint(){
+        //Paint The Canvas
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, w, h);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(0, 0, w, h);
+
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+
+        if(d == "right") nx++;
+        else if(d == "left") nx--;
+        else if(d == "up") ny--;
+        else if(d == "down") ny++;
+
+        //Collide code
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)){
+            //init();
+            //Insert Final Score
+            $('#final_score').html(score);
+            //Show Overlay
+            $('#overlay').fadeIn(300);
+            return;
+        }
+
+        if(nx == food.x && ny == food.y){
+            var tail = {x: nx, y: ny};
+            score++;
+            //Create Food
+            create_food();
+        } else {
+            var tail = snake_array.pop();
+            tail.x = nx; tail.y = ny;
+        }
+
+        snake_array.unshift(tail);
+
+        for(var i = 0;i < snake_array.length;i++){
+            var c = snake_array[i];
+            paint_cell(c.x,c.y);
+        }
+
+        //Paint Cell
+        paint_cell(food.x,food.y);
+
+        //Check Score
+        checkscore(score);
+
+        //Display Current Score
+        $('#score').html('Your Score: '+score);
+    }
+
+    function paint_cell(x,y){
+        ctx.fillStyle=color;
+        ctx.fillRect(x*cw,y*cw,cw,cw);
+        ctx.strokeStyle="white";
+        ctx.strokeRect(x*cw,y*cw,cw,cw);
+    }
+
+    function check_collision(x, y, array){
+        for(var i = 0;i < array.length;i++){
+            if(array[i.x == x && array[i].y ==y])
+                return true;
+        }
+        return false;
     }
 
 });
